728fef42e09b96038387dac4d83aee8aaff4f4a6
John O. Paul <oyomthojohnpaul@gmail.com>
2018-05-15 11:49:29 +0300

    Add ability to close modal with ESC key

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -1,30 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Button from './Button';
 
-const Modal = props => (
-  <div
-    className={`modal ${props.active ? 'modal--active' : ''}`}
-    onClick={props.closeModal}
-    aria-hidden="true"
-  >
-    <div
-      className="modal__content"
-      onClick={event => event.stopPropagation()}
-      aria-hidden="true"
-    >
-      {props.children}
-    </div>
-  </div>
-);
+class Modal extends Component {
+  static defaultProps = {
+    active: false,
+  };
 
-Modal.defaultProps = {
-  active: false,
-};
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    active: PropTypes.bool,
+    closeModal: PropTypes.func.isRequired,
+  };
 
-Modal.propTypes = {
-  children: PropTypes.node.isRequired,
-  active: PropTypes.bool,
-  closeModal: PropTypes.func.isRequired,
-};
+  componentDidMount() {
+    window.addEventListener('keydown', this.keyBoardClose);
+  }
+
+  keyBoardClose = (event) => {
+    if (event.keyCode === 27 && this.props.active) {
+      this.props.closeModal();
+    }
+  };
+
+  render() {
+    return (
+      <div
+        className={`modal ${this.props.active ? 'modal--active' : ''}`}
+        aria-hidden="true"
+      >
+        <Button className="modal__close" onClick={this.props.closeModal}>
+          <svg className="modal__closeIcon" viewBox="0 0 20 20">
+            <line x1="1" y1="1" x2="19" y2="19" />
+            <line x1="19" y1="1" x2="1" y2="19" />
+          </svg>
+          <span className="modal__shortcut">Esc</span>
+        </Button>
+        <div
+          className="modal__content"
+          onClick={event => event.stopPropagation()}
+          aria-hidden="true"
+        >
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Modal;
